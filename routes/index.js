2c49a676b7452b6c814f130ace6ce8f4086c1551
Tim Paul <contact@timpaul.co.uk>
2016-12-09 08:52:34 +0000

    Draft post

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,29 @@ var router = express.Router();
 // Data
 var data = {
 	jobs: [
-		{id: 1, points: 1, title: "Take the bins out"},
-		{id: 2, points: 1, title: "Bring the bins in"},
-		{id: 3, points: 1, title: "Load the dishwasher"},
-		{id: 4, points: 1, title: "Unload the dishwasher"},
-		{id: 5, points: 1, title: "Cook a meal"},
-		{id: 6, points: 1, title: "Do the ironing"},
-		{id: 7, points: 1, title: "Put a load of washing on"},
-		{id: 8, points: 1, title: "Unload the washing"},
-		{id: 9, points: 1, title: "Hang out the washing"},
-		{id: 10, points: 1, title: "Mow the lawn"},
-		{id: 11, points: 1, title: "Do the gardening"},
-		{id: 12, points: 1, title: "Do a tip run"},
-		{id: 13, points: 1, title: "Do the hovering"},
-		{id: 14, points: 1, title: "Drop someone off"},
-		{id: 15, points: 1, title: "Pick someone up"},
-		{id: 16, points: 1, title: "Do an online shop"},
-		{id: 17, points: 1, title: "Do a supermarket shop"},
-		{id: 18, points: 1, title: "Tidy a room"},
-		{id: 19, points: 1, title: "Clean a room"},
-		{id: 20, points: 1, title: "Do some homework"},
-		{id: 21, points: 1, title: "Practice an instrument"},
-		{id: 22, points: 1, title: "Feed Boris"},
-		{id: 22, points: 1, title: "Clean Boris's table"}
+		{id: 1, points: 1, title: "took the bins out"},
+		{id: 2, points: 1, title: "brought the bins in"},
+		{id: 3, points: 1, title: "loaded the dishwasher"},
+		{id: 4, points: 1, title: "unloaded the dishwasher"},
+		{id: 5, points: 1, title: "cooked a meal"},
+		{id: 6, points: 1, title: "did the ironing"},
+		{id: 7, points: 1, title: "put a load of washing on"},
+		{id: 8, points: 1, title: "unloaded the washing"},
+		{id: 9, points: 1, title: "hung out the washing"},
+		{id: 10, points: 1, title: "mowed the lawn"},
+		{id: 11, points: 1, title: "did the gardening"},
+		{id: 12, points: 1, title: "did a tip run"},
+		{id: 13, points: 1, title: "did the hoovering"},
+		{id: 14, points: 1, title: "dropped someone off"},
+		{id: 15, points: 1, title: "picked someone up"},
+		{id: 16, points: 1, title: "did an online shop"},
+		{id: 17, points: 1, title: "did a supermarket shop"},
+		{id: 18, points: 1, title: "tidied a room"},
+		{id: 19, points: 1, title: "cleaned a room"},
+		{id: 20, points: 1, title: "did some homework"},
+		{id: 21, points: 1, title: "practiced an instrument"},
+		{id: 22, points: 1, title: "fed Boris"},
+		{id: 22, points: 1, title: "cleaned Boris's table"}
 	],
 	users: [
 		{id: 1, name: "Tim"}, 
@@ -75,6 +75,13 @@ router.get('/user-:userId/job-:jobId', function(req, res, next) {
 
   	res.render('job-add', pageData);
 
+});
+
+/* POST add job. */
+router.post('/add-job', function(req, res) {
+  console.log(req.body.userId);
+  console.log(req.body.jobId);
+  res.redirect('/');
 });
 
 module.exports = router;
