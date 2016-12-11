1729088a31b5193657d2c640c687940abdf13d55
Tim Paul <contact@timpaul.co.uk>
2016-12-11 16:58:31 +0000

    Update styles

--- routes/index.js
+++ routes/index.js
@@ -77,11 +77,6 @@ router.get('/user-:userId', function(req, res, next) {
 
 	var pageData = data;
 
-	// Add user data
-  	//var userId = parseInt(req.params.userId);
-	//var user = _.where(data.users, {id: userId});
-	//pageData.user = user[0];
-
 	pageData.currentUserId = parseInt(req.params.userId);
 
 	// Get job history from database
@@ -89,6 +84,7 @@ router.get('/user-:userId', function(req, res, next) {
 
 		// Add job history to page data
     	pageData.history = _.sortBy(jobs, 'jobDate').reverse();
+
 
     	// Create a job history grouped by user
 		var jobsByUser = _(jobs).groupBy('userName');
@@ -98,13 +94,16 @@ router.get('/user-:userId', function(req, res, next) {
 
 			// Sum the points for that user
 			var points = _.reduce(user, function(num, event){
-				return event.jobPoints + num; 
+				return event.jobPoints + num;
 			}, 0);
 
 			// Add total points to each user in page data
 			_.findWhere(pageData.users, {name: key}).points = points;
 
 		});
+
+
+    	console.log(pageData);
 
 		// Sort users by points
     	pageData.users = _.sortBy(pageData.users, 'points').reverse();
@@ -185,7 +184,7 @@ router.get('/delete/event-:eventId', function(req, res, next) {
 
 /* Delete ALL events. */
 router.get('/delete/all-events', function(req, res, next) {
-
+	var pageData = {};
 	Job.remove({}, function (err, job) {
 	    res.redirect('/admin');
 	});
